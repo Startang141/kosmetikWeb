302daad9aa3c9102cca1a1aa85a25be3bbe6a963
startang141m <bintangadiyatma141@gmail.com>
2022-12-14 16:06:06 +0700

    khqwjheq

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Kategori;
 use App\Models\Produk;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Google\Cloud\Storage\StorageClient;
+use Illuminate\Support\Facades\URL;
 
 class ProdukController extends Controller
 {
@@ -35,10 +39,47 @@ class ProdukController extends Controller
         ]);
         $imageName = time() . '.' . $request->gambar->extension();
         $request->gambar->move(public_path('produk'), $imageName);
+        if ($request->file('gambar')) {
+            $image_name = $request->file('gambar');
+            // $image_name = $request->file('gambar')->store('images', 'public');
+            $storage = new StorageClient([
+                'keyFilePath' => public_path('key.json')
+            ]);
+
+            $bucketName = env('GOOGLE_CLOUD_BUCKET');
+            $bucket = $storage->bucket($bucketName);
+
+            //get filename with extension
+            $filenamewithextension = pathinfo($request->file('gambar')->getClientOriginalName(), PATHINFO_FILENAME);
+            // $filenamewithextension = $request->file('gambar')->getClientOriginalName();
+
+            //get filename without extension
+            $filename = pathinfo($filenamewithextension, PATHINFO_FILENAME);
+
+            //get file extension
+            $extension = $request->file('gambar')->getClientOriginalExtension();
+
+            //filename to store
+            $filenametostore = $filename . '_' . uniqid() . '.' . $extension;
+
+            Storage::put('public/uploads/' . $filenametostore, fopen($request->file('gambar'), 'r+'));
+
+            $filepath = storage_path('app/public/uploads/' . $filenametostore);
+
+            $object = $bucket->upload(
+                fopen($filepath, 'r'),
+                [
+                    'predefinedAcl' => 'publicRead'
+                ]
+            );
+
+            // delete file from local disk
+            Storage::delete('public/uploads/' . $filenametostore);
+        }
         Produk::create([
             'id_kategori' => $request->kategori,
             'nama_produk' => $request->nama_produk,
-            'gambar' => $imageName,
+            'gambar' => $filenametostore,
             'stok' => $request->stok,
             'terjual' => 0,
         ]);
